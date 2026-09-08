a930f13378fc19d90b7565be811594471d0fe8d1
alee1616 <maguillenc@uamv.edu.ni>
2026-09-07 19:29:45 -0600

    Elavoración del Resgistrocontroller

--- src/main/java/com/example/ejerciciopractica/RegistroDocumentoApplication.java
+++ src/main/java/com/example/ejerciciopractica/RegistroDocumentoApplication.java
@@ -1,16 +1,23 @@
 package com.example.ejerciciopractica;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class RegistroDocumentoApplication extends Application {
+import java.io.IOException;
 
-    public static void main(String[] args) {
-        launch(args);
+public class RegistroDocumentoApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(RegistroDocumentoApplication.class.getResource("registro.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Registro de Documento");
+        stage.setScene(scene);
+        stage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-
+    public static void main(String[] args) {
+        launch();
     }
 }
--- src/main/java/com/example/ejerciciopractica/RegistroDocumentoController.java
+++ src/main/java/com/example/ejerciciopractica/RegistroDocumentoController.java
@@ -1,5 +1,89 @@
 package com.example.ejerciciopractica;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.util.Optional;
+
 public class RegistroDocumentoController {
+
+    @FXML
+    private TextField txtNombreDocumento;
+
+    @FXML
+    private TextField txtDescripcion;
+
+    @FXML
+    private TextField txtArchivoSelec;
+
+    @FXML
+    private TextField txtDirectorio;
+
+    @FXML
+    private TextField txtResultado;
+
+    @FXML
+    private Button btnSelecArchivo;
+
+
+    @FXML
+    private void selecArchivo(){
+        FileChooser fc = new FileChooser();
+        fc.setTitle("Seleccionar archivo");
+        fc.getExtensionFilters().addAll( new FileChooser.ExtensionFilter("TXT", "*.txt"));
+        File archivo = fc.showOpenDialog(btnSelecArchivo.getScene().getWindow());
+
+        if (archivo != null) {
+            txtArchivoSelec.setText(archivo.getAbsolutePath());
+        }
+
+    }
+
+    @FXML
+    private void selecDirectorio(){
+        DirectoryChooser dc = new DirectoryChooser();
+        dc.setTitle("Seleccionar archivo");
+        File directorio = dc.showDialog(btnSelecArchivo.getScene().getWindow());
+
+        if (directorio != null) {
+            txtDirectorio.setText(directorio.getAbsolutePath());
+        }
+
+    }
+
+    @FXML
+    private void mostrarConfirmacion(){
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Confirmación");
+        alert.setHeaderText("¿Desea guardar el documento?");
+        alert.setContentText(txtNombreDocumento.getText());
+
+        Optional<ButtonType> respuesta = alert.showAndWait();
+        if (respuesta.isPresent() && respuesta.get() == ButtonType.OK){
+            mostrarResultado();
+        }
+
+    }
+
+    @FXML
+    private void mostrarAlerta(){
+
+    }
+
+    @FXML
+    private void Guardar(){
+
+    }
+
+
+
+
+
 
 }
