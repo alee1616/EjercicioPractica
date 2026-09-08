811f0368bfb56e24d91dd76b5d853deac05c4b59
alee1616 <maguillenc@uamv.edu.ni>
2026-09-07 18:40:55 -0600

    mejora en scene Builder

---
+++ src/main/java/com/example/ejerciciopractica/RegistroDocumentoApplication.java
@@ -0,0 +1,16 @@
+package com.example.ejerciciopractica;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RegistroDocumentoApplication extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/main/java/com/example/ejerciciopractica/RegistroDocumentoController.java
@@ -0,0 +1,5 @@
+package com.example.ejerciciopractica;
+
+public class RegistroDocumentoController {
+
+}
